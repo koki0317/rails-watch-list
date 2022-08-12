773509c5349f1805f213936e967f9bb3ea842fd6
Koki Takahashi <kokita.0317@gmail.com>
2022-08-12 11:37:56 +0900

    Add a route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # get '/lists/new', to: 'lists#new', as: :new_list
   # get '/lists/:id', to: 'lists#show', as: :list
   # post '/lists', to: 'lists#create'
+  root 'lists#index'
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
